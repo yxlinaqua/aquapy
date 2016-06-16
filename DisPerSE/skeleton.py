c317180ae4058038bd83628a096c7c9961d641c6
Bo Zhang <bozhang@nao.cas.cn>
2016-06-16 14:14:07 +0800

    init skeleton.py

---
+++ DisPerSE/skeleton.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+
+Author
+------
+Bo Zhang
+
+Email
+-----
+bozhang@nao.cas.cn
+
+Created on
+----------
+- Thu Jun  16 14:12:00 2016     init
+
+Modifications
+-------------
+- Thu Jun  16 14:12:00 2016     init
+
+Aims
+----
+- read NDskeleton result text file
+
+"""
+
+import numpy as np
+from astropy.table import Table
+
+
+
+
+
+
